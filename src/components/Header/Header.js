08769acad7297e125d9f59deaa329f84a08917d1
sa-sabuj <sasabuj321@gmail.com>
2022-04-06 01:18:08 +0600

    project initialize

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-5xl'>Welcome to food blogs</h1>
+        </div>
+    );
+};
+
+export default Header;
